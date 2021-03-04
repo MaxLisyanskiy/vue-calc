fda10cf7841bc8d2127847daa6d0c9e95621a422
Max <max_lisyanskiy@mail.ru>
2021-03-04 22:58:56 +0300

    nav

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+
+
 Vue.createApp({
     data() {
         return {
+            nav: ['Главная', "О нас", "Портфолио", "Контакты"],
+            title: 'Главная',
+            isActive: 'Home',
             result: '',
             numbers: [1,2,3,4,5,6,7,8,9],
             operators: ['*', '/', '+', '-']
@@ -16,6 +21,18 @@ Vue.createApp({
         calc() {
             this.result = eval(this.result);
             this.result = String(this.result);
+        },
+        changedTitle(value) {
+            this.title = value;
+            if(value == 'Главная') {
+                this.isActive = 'Home';
+            } else if (value == 'О нас') {
+                this.isActive = 'About';
+            } else if (value == 'Портфолио') {
+                this.isActive = 'Works';
+            } else if (value == 'Контакты') {
+                this.isActive = 'Contacts';
+            }
         }
     }
 }).mount('#app');
